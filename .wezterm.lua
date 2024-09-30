d48287619b4b42192bab606cda3459b3f34d8e73
NachoNievaG <jose.ign.nieva@gmail.com>
2024-09-30 12:20:09 -0300

    feat(tmux): tokyo night

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,20 +1,20 @@
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.font_size = 22
 config.window_background_opacity = 0.7
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
 config.hide_tab_bar_if_only_one_tab = true
 
 config.keys = {
-	{
-		key = "Enter",
-		mods = "ALT",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
+  {
+    key = 'Enter',
+    mods = 'ALT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },
 }
 
-config.color_scheme = "Gruvbox dark, hard (base16)"
+config.color_scheme = 'tokyonight_night'
 config.force_reverse_video_cursor = true
 return config
